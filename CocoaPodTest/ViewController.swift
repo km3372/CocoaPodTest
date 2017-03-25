38b71e51f873a358f2ff51b8b0a48fc0e0e5470d
Ken Moody <km3372@gmail.com>
2017-03-25 06:36:26 -0400

    First Commit

--- CocoaPodTest/ViewController.swift
+++ CocoaPodTest/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       AFURLSessionManager
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
